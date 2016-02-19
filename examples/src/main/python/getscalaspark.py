bd234972080a429cba7c27e80a44894cbdd0c47e
Leonid133 <lenin133@yandex.ru>
2016-02-19 13:21:34 +0300

    add except

--- examples/src/main/python/getscalaspark.py
+++ examples/src/main/python/getscalaspark.py
@@ -21,48 +21,73 @@ from pyspark.sql import SQLContext, HiveContext, SchemaRDD, Row
 
 ###################################################################
 def getSparkContext():
-     client = GatewayClient(port=int(25333))
-     gateway = JavaGateway(client, auto_convert = True)
-     entry_point = gateway.entry_point
+  try:
+    client = GatewayClient(port=int(25333))
+    gateway = JavaGateway(client, auto_convert = True)
+    entry_point = gateway.entry_point
 
-     java_import(gateway.jvm, "org.apache.spark.SparkContext")
-     java_import(gateway.jvm, "org.apache.spark.SparkEnv")
-     java_import(gateway.jvm, "org.apache.spark.SparkConf")
-     java_import(gateway.jvm, "org.apache.spark.api.java.*")
-     java_import(gateway.jvm, "org.apache.spark.api.python.*")
-     java_import(gateway.jvm, "org.apache.spark.mllib.api.python.*")
-     java_import(gateway.jvm, "org.apache.spark.*")
+    java_import(gateway.jvm, "org.apache.spark.SparkContext")
+    java_import(gateway.jvm, "org.apache.spark.SparkEnv")
+    java_import(gateway.jvm, "org.apache.spark.SparkConf")
+    java_import(gateway.jvm, "org.apache.spark.api.java.*")
+    java_import(gateway.jvm, "org.apache.spark.api.python.*")
+    java_import(gateway.jvm, "org.apache.spark.mllib.api.python.*")
+    java_import(gateway.jvm, "org.apache.spark.*")
      
 
-     ScalaSparkContextWrapper = entry_point.ScalaSparkContextWrapper()
-     sconf = ScalaSparkContextWrapper.getSparkConf()
-     conf = SparkConf(_jvm = gateway.jvm, _jconf = sconf)
-     jsc = ScalaSparkContextWrapper.getSparkContext()
-     sc = SparkContext(jsc=jsc, gateway=gateway, conf=conf) 
-     return sc
+    ScalaSparkContextWrapper = entry_point.ScalaSparkContextWrapper()
+    sconf = ScalaSparkContextWrapper.getSparkConf()
+    conf = SparkConf(_jvm = gateway.jvm, _jconf = sconf)
+    jsc = ScalaSparkContextWrapper.getSparkContext()
+    sc = SparkContext(jsc=jsc, gateway=gateway, conf=conf) 
+    return sc
+
+  except Py4JJavaError:
+    print("except Py4JJavaError")
+    return None  
+  
+  except Exception:
+    print("except")
+    return None
 
 def getNumbers():
-     client = GatewayClient(port=int(25333))
-     gateway = JavaGateway(client, auto_convert = True)
-     entry_point = gateway.entry_point
-     java_import(gateway.jvm,'java.util.*')
-     SimpleDataWrapper = entry_point.SimpleDataWrapper()
+  try:
+    client = GatewayClient(port=int(25333))
+    gateway = JavaGateway(client, auto_convert = True)
+    entry_point = gateway.entry_point
+    java_import(gateway.jvm,'java.util.*')
+    SimpleDataWrapper = entry_point.SimpleDataWrapper()
 
-     num = SimpleDataWrapper.get()
-     l = list()
-     count = 0
-     size = num.size()
-     while count < size:
-          l.append(num.head())
-          count = count + 1
-          num = num.tail()
-     return l
+    num = SimpleDataWrapper.get()
+    l = list()
+    count = 0
+    size = num.size()
+    while count < size:
+      l.append(num.head())
+      count = count + 1
+      num = num.tail()
+    return l
+
+  except Py4JJavaError:
+    print("except Py4JJavaError")
+    return None 
+  
+  except Exception:
+    print("except")
+    return None
 
 def sendResult(result):
-     client = GatewayClient(port=int(25333))
-     gateway = JavaGateway(client, auto_convert = True)
-     entry_point = gateway.entry_point
-     java_import(gateway.jvm,'java.util.*')
-     SimpleDataWrapper = entry_point.SimpleDataWrapper()
-     SimpleDataWrapper.set(result)
+  try:
+    client = GatewayClient(port=int(25333))
+    gateway = JavaGateway(client, auto_convert = True)
+    entry_point = gateway.entry_point
+    java_import(gateway.jvm,'java.util.*')
+    SimpleDataWrapper = entry_point.SimpleDataWrapper()
+    SimpleDataWrapper.set(result)
+
+  except Py4JJavaError:
+    print("except Py4JJavaError")
+
+  except Exception:
+    print("except")
 
