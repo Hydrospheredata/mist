a96184c8bdcf4c06dd96f478ac35dc9c063761bd
Leonid133 <lenin133@yandex.ru>
2016-04-15 16:58:24 +0300

    Fix MQTT tests
    Former-commit-id: 22497e6094eb1fa91f49566959c582fd9a293239

--- src/test/scala/TestMqttSubPub.scala
+++ src/test/scala/TestMqttSubPub.scala
@@ -2,9 +2,13 @@ package io.hydrosphere.mist.test
 
 import java.net.{InetAddress, InetSocketAddress}
 
-import akka.actor.{ActorRef, Actor}
+import akka.actor.{ActorSystem, Props, ActorRef, Actor}
+
 import io.hydrosphere.mist.MistConfig
 //import net.sigusr.mqtt.api._
+
+import org.eclipse.paho.client.mqttv3._
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
 object MqttSuccessObj{
   var success: Boolean = false
@@ -12,57 +16,44 @@ object MqttSuccessObj{
   var ready_sub: Boolean = false
 }
 
-//class MQTTTestSub extends Actor {
-//  val mqttClient = context.actorOf(Manager.props(new InetSocketAddress(InetAddress.getByName(MistConfig.MQTT.host), MistConfig.MQTT.port)))
-//  mqttClient ! Connect(TestConfig.mqtt_test_sub_name)
-//
-//  override def receive: Receive = {
-//
-//    case Connected =>
-//      println("Sub connected to mqtt")
-//      sender() ! Subscribe(Vector((MistConfig.MQTT.subscribeTopic, AtMostOnce)), 1)
-//      context become ready(sender())
-//
-//    case ConnectionFailure(reason) => println(s"Sub connection to mqtt failed [$reason]")
-//
-//  }
-//
-//  def ready(mqttManager: ActorRef): Receive = {
-//
-//    case Subscribed(vQoS, MessageId(1)) => {
-//      println("Test sub successfully subscribed")
-//      MqttSuccessObj.ready_sub = true
-//    }
-//
-//    case Message(topic, payload) =>
-//
-//      val stringMessage = new String(payload.to[Array], "UTF-8")
-//      println(s"[$topic] $stringMessage")
-//      stringMessage.split(':').drop(1).head.split(',').headOption.getOrElse("false") match {
-//        case "true" =>  MqttSuccessObj.success = true
-//        case "false" => MqttSuccessObj.success = false
-//        case _ =>
-//      }
-//  }
-//}
-//
-//class MQTTTestPub extends Actor {
-//
-//  val mqttClient = context.actorOf(Manager.props(new InetSocketAddress(InetAddress.getByName(MistConfig.MQTT.host), MistConfig.MQTT.port)))
-//  mqttClient ! Connect(TestConfig.mqtt_test_pub_name)
-//
-//  override def receive: Receive = {
-//
-//    case Connected =>
-//      println("Publisher connected to mqtt")
-//      MqttSuccessObj.ready_pub = true
-//      context become ready(sender())
-//
-//    case ConnectionFailure(reason) => println(s"Pub connection to mqtt failed [$reason]")
-//  }
-//
-//  def ready(mqttManager: ActorRef): Receive = {
-//
-//    case msg: String => mqttClient ! Publish(MistConfig.MQTT.publishTopic, msg.getBytes("UTF-8").to[Vector])
-//  }
-//}
+object MQTTTest{
+  def publish(message: String) = {
+    var client: MqttClient = null
+    val persistence = new MemoryPersistence
+    try {
+      client = new MqttClient(s"tcp://${MistConfig.MQTT.host}:${MistConfig.MQTT.port}", MqttClient.generateClientId, persistence)
+      client.connect
+      val msgTopic = client.getTopic(MistConfig.MQTT.publishTopic)
+      val mqMessage = new MqttMessage(message.getBytes("utf-8"))
+      msgTopic.publish(mqMessage)
+      println(s"Publishing Data, Topic : ${msgTopic.getName}, Message : $mqMessage")
+    }
+  }
+
+  def subscribe(actorSystem: ActorSystem) = {
+    val persistence = new MemoryPersistence
+    val mqttClient = new MqttClient(s"tcp://${MistConfig.MQTT.host}:${MistConfig.MQTT.port}", MqttClient.generateClientId, persistence)
+    mqttClient.connect
+    mqttClient.subscribe(MistConfig.MQTT.subscribeTopic)
+    val callback = new MqttCallback {
+      override def messageArrived(topic: String, message: MqttMessage): Unit = {
+        println("Receiving Data, Topic : %s, Message : %s".format(topic, message))
+        var stringMessage = message.toString
+        stringMessage.split(':').drop(1).head.split(',').headOption.getOrElse("false") match {
+          case "true" => MqttSuccessObj.success = true
+          case "false" => MqttSuccessObj.success = false
+          case _ =>
+        }
+      }
+
+      override def connectionLost(cause: Throwable): Unit = {
+        println(cause)
+      }
+
+      override def deliveryComplete(token: IMqttDeliveryToken): Unit = {
+      }
+    }
+    mqttClient.setCallback(callback)
+  }
+}
+
