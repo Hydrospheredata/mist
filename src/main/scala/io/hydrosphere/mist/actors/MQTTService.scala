f447fde949b3f82d94d44090bfd6d239d6feab42
Leonid133 <lenin133@yandex.ru>
2016-05-20 20:38:25 +0300

    Ref Mqtt on Actors

--- src/main/scala/io/hydrosphere/mist/actors/MQTTService.scala
+++ src/main/scala/io/hydrosphere/mist/actors/MQTTService.scala
@@ -1,156 +1,239 @@
 package io.hydrosphere.mist.actors
 
-import akka.actor.{ActorSystem, Props, ActorRef, Actor}
+import akka.actor._
 import akka.pattern.ask
 import io.hydrosphere.mist.{Constants, MistConfig}
-
 import org.json4s.DefaultFormats
 import org.json4s.native.Json
 import spray.json._
 
-import scala.util.{Failure, Success}
 import scala.concurrent.ExecutionContext.Implicits.global
-
-import io.hydrosphere.mist.jobs.{JobResult, JobConfiguration}
+import io.hydrosphere.mist.jobs.{JobConfiguration, JobResult}
 import org.eclipse.paho.client.mqttv3._
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
+import scala.concurrent.duration._
+import scala.collection.mutable.ListBuffer
 
-//TODO: must be an akka actor
-private[mist] object MQTTService extends DefaultJsonProtocol {
+private[mist] object MqttPubSub {
 
-  // TODO: remove copy/paste from HTTP actor
-  implicit object AnyJsonFormat extends JsonFormat[Any] {
-    def write(x: Any) = x match {
-      case number: Int => JsNumber(number)
-      case string: String => JsString(string)
-      case sequence: Seq[_] => seqFormat[Any].write(sequence)
-      case map: Map[String, _] => mapFormat[String, Any] write map
-      case boolean: Boolean if boolean => JsTrue
-      case boolean: Boolean if !boolean => JsFalse
-      case unknown => serializationError("Do not understand object of type " + unknown.getClass.getName)
-    }
-    def read(value: JsValue) = value match {
-      case JsNumber(number) => number.toBigInt()
-      case JsString(string) => string
-      case array: JsArray => listFormat[Any].read(value)
-      case jsObject: JsObject => mapFormat[String, Any].read(value)
-      case JsTrue => true
-      case JsFalse => false
-      case unknown => deserializationError("Do not understand how to deserialize " + unknown)
+  private[mist] class Publish(payload: Array[Byte]) {
+    def message() = {
+      val msg = new MqttMessage(payload)
+      msg
     }
   }
 
-  implicit val jobCreatingRequestFormat = jsonFormat6(JobConfiguration)
+  private[mist] case class Subscribe(ref: ActorRef)
 
-  def randomStringFromCharList(length: Int, chars: Seq[Char]): String = {
-    val sb = new StringBuilder
-    for (i <- 1 to length) {
-      val randomNum = scala.util.Random.nextInt(chars.length)
-      sb.append(chars(randomNum))
-    }
-    sb.toString
-  }
-  def randomAlphaNumericString(length: Int): String = {
-    val chars = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
-    randomStringFromCharList(length, chars)
-  }
+  private[mist] case class SubscribeAck(subscribe: Subscribe)
 
-  def publish(message: String) = {
-    var client: MqttClient = null
+  private[mist] class Message(val payload: Array[Byte])
 
-    val persistence = new MemoryPersistence
+  private[mist] case object Connect
+  private[mist] case object Connected
+  private[mist] case object Disconnected
 
-    try {
-      // mqtt client with specific url and client id
+  private[mist] class Subscribers extends Actor {
+    private var subscribers = Set.empty[ActorRef]
 
-      client = new MqttClient(s"tcp://${MistConfig.MQTT.host}:${MistConfig.MQTT.port}", randomAlphaNumericString(16), persistence)
+    def receive = {
+      case msg: Message =>
+        subscribers foreach (_ ! msg)
 
-      client.connect
+      case msg @ Subscribe(ref) =>
+        context watch ref
+        subscribers += ref
+        ref ! SubscribeAck(msg)
 
-      val msgTopic = client.getTopic(MistConfig.MQTT.publishTopic)
-      val mqMessage = new MqttMessage(message.getBytes("utf-8"))
-
-      msgTopic.publish(mqMessage)
-      println(s"Publishing Data, Topic : ${msgTopic.getName}, Message : $mqMessage")
-    }
-
-    catch {
-      case e: MqttException => println("Exception Caught: " + e)
-    }
-
-    finally {
-      client.disconnect
+      case Terminated(ref) =>
+        subscribers -= ref
+        if (subscribers.isEmpty) context stop self
     }
   }
 
-  def subscribe(actorSystem: ActorSystem) = {
-    lazy val jobRequestActor: ActorRef = actorSystem.actorOf(Props[JobRunner], name = Constants.Actors.asyncJobRunnerName)
+  private[mist] class PubSubMqttCallback(owner: ActorRef) extends MqttCallback {
 
-    val persistence = new MemoryPersistence
+    def connectionLost(cause: Throwable): Unit = {
+      println("connection lost")
+      owner ! Disconnected
+    }
 
-    val client = new MqttClient(s"tcp://${MistConfig.MQTT.host}:${MistConfig.MQTT.port}", randomAlphaNumericString(16), persistence)
+    def deliveryComplete(token: IMqttDeliveryToken): Unit = {
 
-    client.connect
+    }
 
-    client.subscribe(MistConfig.MQTT.subscribeTopic)
+    def messageArrived(topic: String, message: MqttMessage): Unit = {
+      owner ! new Message(message.getPayload)
+    }
+  }
 
-    val callback = new MqttCallback {
-      override def messageArrived(topic: String, message: MqttMessage): Unit = {
-        println("Receiving Data, Topic : %s, Message : %s".format(topic, message))
+  private[mist] class ConnectionListener(owner: ActorRef) extends IMqttActionListener {
 
-        val stringMessage = message.toString
+    def onSuccess(asyncActionToken: IMqttToken): Unit = {
+      owner ! Connected
+    }
 
-          val json = {
-            try {
-              stringMessage.parseJson
-            }
-            catch {
-              case _: spray.json.JsonParser.ParsingException => {
-                return
-              } // pass invalid json
-            }
+    def onFailure(asyncActionToken: IMqttToken, e: Throwable): Unit = {
+      owner ! Disconnected
+    }
+  }
+
+  private[mist] object SubscribeListener extends IMqttActionListener {
+    def onSuccess(asyncActionToken: IMqttToken): Unit = {
+      println("subscribed to " + asyncActionToken.getTopics.mkString("[", ",", "]"))
+    }
+
+    def onFailure(asyncActionToken: IMqttToken, e: Throwable): Unit = {
+      println("subscribe failed to " + asyncActionToken.getTopics.mkString("[", ",", "]"))
+    }
+  }
+}
+
+import MqttPubSub._
+
+private[mist] class MqttPubSub(connectionUrl: String) extends Actor{
+
+  lazy val connectionOptions = {
+    val opt = new MqttConnectOptions
+    opt.setCleanSession(true)
+    opt
+  }
+
+  private[this] val client = {
+    val c = new MqttAsyncClient(connectionUrl, MqttAsyncClient.generateClientId(), null)
+    c.setCallback(new PubSubMqttCallback(self))
+    c
+  }
+
+  private[this] val connectionListener = new ConnectionListener(self)
+
+  private[this] val msgBuffer = ListBuffer.empty[(Deadline, Any)]
+
+  def receive = {
+    case Connect => {
+      println(s"connecting to ${connectionUrl}..")
+      try {
+        client.connect(connectionOptions, null, connectionListener)
+      } catch {
+        case e: Exception => println(s"can't connect to $connectionUrl")
+      }
+    }
+    case Connected => {
+      for ((deadline, x) <- msgBuffer if deadline.hasTimeLeft()) self ! x
+      msgBuffer.clear()
+      context become ready
+    }
+
+    case x @ (_: Publish | _: Subscribe) => {
+      if (msgBuffer.length > 1000) {
+        msgBuffer.remove(0, 500)
+      }
+      msgBuffer += Tuple2(Deadline.now + 1.day, x)
+    }
+  }
+
+  def ready : Receive  = {
+
+    case p: Publish => {
+      try {
+        client.publish(MistConfig.MQTT.publishTopic, p.message())
+      } catch {
+        case e: Exception => println(s"can't publish to ${MistConfig.MQTT.publishTopic}")
+      }
+    }
+
+    case msg@Subscribe(ref) => {
+
+      context.child(Constants.Actors.mqttServiceName) match {
+        case Some(t) => t ! msg
+        case None =>
+          val t = context.actorOf(Props[Subscribers], name = Constants.Actors.mqttServiceName)
+          t ! msg
+          context watch t
+          try {
+            client.subscribe(MistConfig.MQTT.publishTopic, 0, null, SubscribeListener)
+          } catch {
+            case e: Exception => println(e); println(s"can't subscribe to ${MistConfig.MQTT.publishTopic}")
           }
-        
-          // map request into JobConfiguration
-          val jobCreatingRequest = {
-            try {
-              json.convertTo[JobConfiguration]
-            } catch {
-              case _: DeserializationException => return // pass invalid json
-            }
-          }
-
-          // Run job asynchronously
-          val future = jobRequestActor.ask(jobCreatingRequest)(timeout = MistConfig.Contexts.timeout(jobCreatingRequest.name))
-
-          future
-            .recover {
-              case error: Throwable => Right(error.toString)
-            }
-            .onSuccess {
-              case result: Either[Map[String, Any], String] =>
-                val jobResult: JobResult = result match {
-                  case Left(jobResult: Map[String, Any]) =>
-                    JobResult(success = true, payload = jobResult, request = jobCreatingRequest, errors = List.empty)
-                  case Right(error: String) =>
-                    JobResult(success = false, payload = Map.empty[String, Any], request = jobCreatingRequest, errors = List(error))
-                }
-
-                val jsonString = Json(DefaultFormats).write(jobResult)
-                MQTTService.publish(jsonString)
-            }
-      }
-
-      override def connectionLost(cause: Throwable): Unit = {
-        println(cause)
-      }
-
-      override def deliveryComplete(token: IMqttDeliveryToken): Unit = {
-
       }
     }
 
-    client.setCallback(callback)
+    case msg: Message => context.child(Constants.Actors.mqttServiceName) foreach (_ ! msg)
+
+    case Terminated(topicRef) => {
+      try {
+        client.unsubscribe(Constants.Actors.mqttServiceName)
+      } catch {
+        case e: Exception => println(s"can't unsubscribe from ${topicRef.path.name}")
+      }
+    }
+
+    case Disconnected => {
+      context become receive
+      self ! Connect
+    }
+  }
+
+  self ! Connect
+}
+
+private[mist] case object MqttSubscribe
+
+
+private[mist] trait MqttPubSubActor { this: Actor =>
+  val pubsub = context.actorOf(Props(classOf[MqttPubSub], (s"tcp://${MistConfig.MQTT.host}:${MistConfig.MQTT.port}")))
+}
+
+private[mist] class MQTTServiceActor extends Actor with MqttPubSubActor with JsonFormatSupport{
+
+  lazy val jobRequestActor: ActorRef = context.actorOf(Props[JobRunner], name = Constants.Actors.asyncJobRunnerName)
+
+  def receive = {
+
+    case MqttSubscribe => pubsub ! Subscribe(self)
+
+    case msg @ SubscribeAck(Subscribe(`self`)) => context become ready
+  }
+
+  def ready: Receive = {
+
+    case msg: Message => {
+
+      val stringMessage = new String(msg.payload, "utf-8")
+
+      println("Receiving Data, Topic : %s, Message : %s".format(MistConfig.MQTT.publishTopic, stringMessage))
+      try {
+        val json = stringMessage.parseJson
+        // map request into JobConfiguration
+        val jobCreatingRequest = json.convertTo[JobConfiguration]
+
+        // Run job asynchronously
+        val future = jobRequestActor.ask(jobCreatingRequest)(timeout = MistConfig.Contexts.timeout(jobCreatingRequest.name))
+
+        future
+          .recover {
+            case error: Throwable => Right(error.toString)
+          }
+          .onSuccess {
+            case result: Either[Map[String, Any], String] =>
+              val jobResult: JobResult = result match {
+                case Left(jobResult: Map[String, Any]) =>
+                  JobResult(success = true, payload = jobResult, request = jobCreatingRequest, errors = List.empty)
+                case Right(error: String) =>
+                  JobResult(success = false, payload = Map.empty[String, Any], request = jobCreatingRequest, errors = List(error))
+              }
+
+              val jsonString = Json(DefaultFormats).write(jobResult)
+              pubsub ! new Publish(jsonString.getBytes("utf-8"))
+          }
+      }
+      catch {
+
+        case _: spray.json.JsonParser.ParsingException => println("BadJson")
+
+        case _: DeserializationException => println("DeserializationException Bad type in Json")
+      }
+
+    }
   }
 }
