2bf2ba8cae7019f68d79e74b737ea05307f6e0b6
Leonid133 <lenin133@yandex.ru>
2016-06-28 19:50:44 +0300

    ref JobRepository
    Former-commit-id: 12120ba8d6f4667e463b507c6ca88d402edba629
    (cherry picked from commit 5130595)

--- src/main/scala/io/hydrosphere/mist/jobs/JobRepository.scala
+++ src/main/scala/io/hydrosphere/mist/jobs/JobRepository.scala
@@ -5,57 +5,39 @@ import io.hydrosphere.mist.{Repository, Specification, MistConfig, Constants, Ma
 import org.apache.commons.lang.SerializationUtils
 import org.mapdb.{DBMaker, Serializer}
 
-import scala.collection.mutable.ArrayBuffer
-
-private[mist] trait JobRepository extends Repository[Job]
-
-private[mist] object InMemoryJobRepository extends JobRepository {
-
-  private val _collection = ArrayBuffer.empty[Job]
-
-  override def add(job: Job): Unit = {
-    _collection += job
-  }
-
-  override def get(specification: Specification[Job]): Option[Job] = {
-    val predicate: Job => Boolean = x => specification.specified(x)
-    _collection.find(predicate)
-  }
-
-  override def filter(specification: Specification[Job]): List[Job] = {
-    val predicate: Job => Boolean = x => specification.specified(x)
-    _collection.filter(predicate).toList
-  }
-
-  override def remove(job: Job): Unit = {
-    _collection -= job
-  }
-}
 
 private[mist] trait ConfigurationRepository {
-  def add(job: Job): Unit = ???
-  def remove(job: Job): Unit = ???
-  def getAll: ArrayBuffer[JobConfiguration] = ???
+  def add(jobId: String, jobConfiguration: JobConfiguration): Unit = ???
+  def remove(jobId: String): Unit = ???
+  def get(jobId: String): JobConfiguration = ???
+  def getAll: scala.collection.mutable.Map[String, JobConfiguration] = ???
+  def size: Int = ???
   def clear(): Unit = ???
 }
 
 private[mist] object InMemoryJobConfigurationRepository extends ConfigurationRepository {
 
-  private val _collection = ArrayBuffer.empty[JobConfiguration]
+  private val _collection = scala.collection.mutable.Map[String, JobConfiguration]()
 
-  override def add(job: Job): Unit = {
-    _collection += job.configuration
+  override def add(jobId: String, jobConfiguration: JobConfiguration): Unit = {
+    _collection put (jobId, jobConfiguration)
   }
 
-  override def remove(job: Job): Unit = {
-    _collection -= job.configuration
+  override def remove(jobId: String): Unit = {
+    _collection - jobId
   }
 
-  override def getAll: ArrayBuffer[JobConfiguration] = _collection
+  override def get(jobId: String): JobConfiguration = {
+    _collection(jobId)
+  }
+
+  override def getAll: scala.collection.mutable.Map[String, JobConfiguration] = _collection
+
+  override def size: Int = _collection.size
 
   override def clear(): Unit = {
     for(jobConf <- getAll){
-      _collection -= jobConf
+      _collection - jobConf._1
     }
   }
 }
@@ -76,17 +58,7 @@ private[mist] object InMapDbJobConfigurationRepository extends ConfigurationRepo
   // Json formats
   private implicit val formats = org.json4s.DefaultFormats
 
-  override def add(job: Job): Unit = {
-    try {
-      val w_job = SerializationUtils.serialize(job.configuration)
-      map.put(job.id, w_job)
-      println(s"${job.id} saved in MapDb")
-    } catch {
-      case e: Exception => println(e)
-    }
-  }
-
-  def addById(jobId: String, jobConfiguration: JobConfiguration): Unit = {
+  override def add(jobId: String, jobConfiguration: JobConfiguration): Unit = {
     try {
       val w_job = SerializationUtils.serialize(jobConfiguration)
       map.put(jobId, w_job)
@@ -96,16 +68,7 @@ private[mist] object InMapDbJobConfigurationRepository extends ConfigurationRepo
     }
   }
 
-  override def remove(job: Job): Unit = {
-    try {
-      map.remove(job.id)
-      println(s"${job.id} removed from MapDb")
-    } catch{
-      case e: Exception => println(e)
-    }
-  }
-
-  def removeById(jobId: String): Unit = {
+  override def remove(jobId: String): Unit = {
     try {
       map.remove(jobId)
       println(s"${jobId} removed from MapDb")
@@ -114,35 +77,36 @@ private[mist] object InMapDbJobConfigurationRepository extends ConfigurationRepo
     }
   }
 
-  override def getAll: ArrayBuffer[JobConfiguration] = {
+  override def getAll: scala.collection.mutable.Map[String, JobConfiguration] = {
+    val _collection = scala.collection.mutable.Map[String, JobConfiguration]()
     try{
       val keys = map.getKeys.toArray()
-      var _collection = ArrayBuffer.empty[JobConfiguration]
+
       for(key <- keys){
-        _collection += SerializationUtils.deserialize(map.get(key.toString)).asInstanceOf[JobConfiguration]
+        _collection put (key.toString, SerializationUtils.deserialize(map.get(key.toString)).asInstanceOf[JobConfiguration])
       }
       println(s"${_collection.size} loaded from MapDb")
-      _collection
     }
     catch {
       case e: Exception =>
         println(e)
-        ArrayBuffer.empty[JobConfiguration]
     }
+    _collection
   }
 
-  def checkById(jobId: String): Boolean = {
+  override def get(jobId: String): JobConfiguration = {
+    var jobConfiguration: JobConfiguration = ???
     try {
-      val jobConfiguration = SerializationUtils.deserialize(map.get(jobId)).asInstanceOf[JobConfiguration]
-      jobConfiguration.name.nonEmpty
+      jobConfiguration = SerializationUtils.deserialize(map.get(jobId)).asInstanceOf[JobConfiguration]
     }
     catch {
       case e: Exception =>
-        false
+        println(e)
     }
+    jobConfiguration
   }
 
-  def countAll: Int ={
+  override def size: Int ={
     try{
       val keys = map.getKeys.toArray()
       var _count = 0
@@ -167,62 +131,4 @@ private[mist] object InMapDbJobConfigurationRepository extends ConfigurationRepo
  }
 }
 
-private[mist] object RecoveryJobRepository extends JobRepository {
 
-  private val _collection = ArrayBuffer.empty[Job]
-
-  lazy val configurationRepository = InMapDbJobConfigurationRepository
-  /*
-  lazy val configurationRepository: ConfigurationRepository = MistConfig.Recovery.recoveryTypeDb match {
-    case "MapDb" => InMapDbJobConfigurationRepository
-    case _ => InMemoryJobConfigurationRepository
-
-  }*/
-
-  override def add(job: Job): Unit = {
-    _collection += job
-    if(job.jobRunnerName == Constants.Actors.asyncJobRunnerName) {
-      configurationRepository.add(job)
-      Master.recoveryActor ! JobStarted
-    }
-  }
-
-  def addById(jobId: String, jobConfiguration: JobConfiguration): Unit = {
-    configurationRepository.addById(jobId, jobConfiguration)
-    Master.recoveryActor ! JobStarted
-  }
-
-  def removeById(jobId: String): Unit = {
-    configurationRepository.removeById(jobId)
-    Master.recoveryActor ! JobCompleted
-  }
-
-  override def get(specification: Specification[Job]): Option[Job] = {
-    val predicate: Job => Boolean = x => specification.specified(x)
-    _collection.find(predicate)
-  }
-
-  override def filter(specification: Specification[Job]): List[Job] = {
-    val predicate: Job => Boolean = x => specification.specified(x)
-    _collection.filter(predicate).toList
-  }
-
-  override def remove(job: Job): Unit = {
-    _collection -= job
-  }
-
-  def removeFromRecovery(job: Job): Unit = {
-    if(job.jobRunnerName == Constants.Actors.asyncJobRunnerName) {
-      configurationRepository.remove(job)
-      Master.recoveryActor ! JobCompleted
-    }
-  }
-
-  def checkById(jobId: String): Boolean = {
-    configurationRepository.checkById(jobId)
-  }
-
-  def countAll: Int ={
-    configurationRepository.countAll
-  }
-}
--- src/main/scala/io/hydrosphere/mist/master/JobRecovery.scala
+++ src/main/scala/io/hydrosphere/mist/master/JobRecovery.scala
@@ -13,14 +13,14 @@ import scala.collection.mutable.ArrayBuffer
 case object StartRecovery
 
 case object TryRecoveyNext{
- var _collection = ArrayBuffer.empty[JobConfiguration]
+ var _collection = scala.collection.mutable.Map[String, JobConfiguration]()
 }
 case object JobStarted{
   var jobStartedCount = 0
 }
 case object JobCompleted
 
-private[mist] class JobRecovery(configurationRepository :ConfigurationRepository, jobRepository: JobRepository) extends Actor with MqttPubSubActor {
+private[mist] class JobRecovery(configurationRepository :ConfigurationRepository) extends Actor with MqttPubSubActor {
 
   private implicit val formats = org.json4s.DefaultFormats
 
@@ -36,10 +36,10 @@ private[mist] class JobRecovery(configurationRepository :ConfigurationRepository
       if (JobStarted.jobStartedCount < MistConfig.Recovery.recoveryMultilimit) {
         if (TryRecoveyNext._collection.nonEmpty) {
           val job_configuration = TryRecoveyNext._collection.last
-          val json = Serialization.write(job_configuration)
+          val json = Serialization.write(job_configuration._2)
           println(s"send $json")
           pubsub ! new Publish(json.getBytes("utf-8"))
-          TryRecoveyNext._collection -= TryRecoveyNext._collection.last
+          TryRecoveyNext._collection - TryRecoveyNext._collection.last._1
         }
       }
 
