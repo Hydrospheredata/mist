4e64a62dba01b7c5044c7ca6f42c4e1950f5f42e
Leonid133 <lenin133@yandex.ru>
2016-08-08 18:29:20 +0300

    readme update

--- src/main/scala/io/hydrosphere/mist/contexts/ContextWrapper.scala
+++ src/main/scala/io/hydrosphere/mist/contexts/ContextWrapper.scala
@@ -14,26 +14,6 @@ private[mist] trait ContextWrapper {
 
   private val jars: ArrayBuffer[String] = ArrayBuffer.empty[String]
 
-/*
-  val versionRegex = "(\\d+)\\.(\\d+).*".r
-  val sparkVersion = util.Properties.propOrNone("sparkVersion").getOrElse("[1.5.2, )")
-
-  val checkSparkSessionLogic = {
-    sparkVersion match {
-      case versionRegex(major, minor) if major.toInt > 1 => true
-      case _ => false
-    }
-  }
-
-  lazy val sparkSession = if (checkSparkSessionLogic) {
-     SparkSession
-      .builder()
-      .appName(context.appName)
-      .config(context.getConf)
-      .enableHiveSupport()
-      .getOrCreate()
-  }
-*/
   lazy val sqlContext = new SQLContext(context)
 
   lazy val hiveContext = {
