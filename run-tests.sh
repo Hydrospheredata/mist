a95bfcdc0505f6f97ba59a299818895fd28c2872
Leonid <lenin133@yandex.ru>
2016-08-25 22:39:28 +0300

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -2,30 +2,4 @@ export PYTHONPATH=$MIST_HOME/src/main/python:$SPARK_HOME/python/:`readlink -f $S
 $SPARK_HOME/sbin/start-master.sh
 $SPARK_HOME/sbin/start-slave.sh localhost:7077
 cd $MIST_HOME
-
-app=$1
-
-shift
-while [[ $# > 1 ]]
-do
-    key="$1"
-
-  case ${key} in
-    --sparkVersion)
-      SPARK_VERSION="$2"
-      shift
-      ;;
-  esac
-
-shift
-done
-
-if [ "${SPARK_VERSION}" == '' ] || [ ! -d "${SPARK_VERSION}" ]
-then
-    #echo "SPARK_VERSION is not set"
-    #exit 1
-    SPARK_VERSION = 1.5.2
-fi
-./sbt/sbt -DsparkVersion=${SPARK_VERSION} -Dconfig.file=configs/docker.conf test
-$SPARK_HOME/sbin/stop-master.sh
-$SPARK_HOME/sbin/stop-slave.sh
+./sbt/sbt -DsparkVersion=1.5.2 -Dconfig.file=configs/docker.conf test
