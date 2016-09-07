9d728128db7eef05f2277af69138b99939c15672
Leonid133 <lenin133@yandex.ru>
2016-09-07 12:13:13 +0300

    Http Response content-type application/json

--- src/main/scala/io/hydrosphere/mist/master/HTTPService.scala
+++ src/main/scala/io/hydrosphere/mist/master/HTTPService.scala
@@ -1,11 +1,12 @@
 package io.hydrosphere.mist.master
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.server.{Directives, Route}
+import akka.http.scaladsl.marshalling.{ToResponseMarshallable}
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.stream.ActorMaterializer
 import akka.pattern.{AskTimeoutException, ask}
+import akka.stream.scaladsl.Flow
 import io.hydrosphere.mist.{Constants, Logger, MistConfig}
 import spray.json.{DefaultJsonProtocol, JsArray, JsFalse, JsNumber, JsObject, JsString, JsTrue, JsValue, JsonFormat, deserializationError, serializationError}
 import org.json4s.DefaultFormats
@@ -14,6 +15,9 @@ import io.hydrosphere.mist.jobs.{JobConfiguration, JobResult}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.language.reflectiveCalls
+
+import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 
 private[mist] trait JsonFormatSupport extends DefaultJsonProtocol{
   /** We must implement json parse/serializer for [[Any]] type */
@@ -40,6 +44,7 @@ private[mist] trait JsonFormatSupport extends DefaultJsonProtocol{
 
   // JSON to JobConfiguration mapper (6 fields)
   implicit val jobCreatingRequestFormat = jsonFormat6(JobConfiguration)
+  implicit val jobResultFormat = jsonFormat4(JobResult)
 }
 /** HTTP interface */
 private[mist] trait HTTPService extends Directives with SprayJsonSupport with JsonFormatSupport with Logger{
@@ -48,36 +53,37 @@ private[mist] trait HTTPService extends Directives with SprayJsonSupport with Js
   implicit val materializer: ActorMaterializer
 
   // /jobs
-  def route : Route = path("jobs") {
-    // POST /jobs
-    post {
-      // POST body must be JSON mapable into JobConfiguration
-      entity(as[JobConfiguration]) { jobCreatingRequest =>
-        complete {
+  def route: Flow[HttpRequest, HttpResponse, Unit] =  {
+    path("jobs") {
+      // POST /jobs
+      post {
+          entity(as[JobConfiguration]) { jobCreatingRequest =>
+              respondWithHeader(RawHeader("Content-Type", "application/json"))
+              complete {
+                logger.info(jobCreatingRequest.parameters.toString)
 
-          logger.info(jobCreatingRequest.parameters.toString)
+                // Run job asynchronously
+                val workerManagerActor = system.actorSelection(s"akka://mist/user/${Constants.Actors.workerManagerName}")
 
-          // Run job asynchronously
-          val workerManagerActor = system.actorSelection(s"akka://mist/user/${Constants.Actors.workerManagerName}")
+                val future = workerManagerActor.ask(jobCreatingRequest)(timeout = MistConfig.Contexts.timeout(jobCreatingRequest.name))
 
-          val future = workerManagerActor.ask(jobCreatingRequest)(timeout = MistConfig.Contexts.timeout(jobCreatingRequest.name))
-
-          future
-            .recover {
-              case error: AskTimeoutException => Right(Constants.Errors.jobTimeOutError)
-              case error: Throwable => Right(error.toString)
-            }
-            .map[ToResponseMarshallable] {
-              case result: Either[Map[String, Any], String] =>
-                val jobResult: JobResult = result match {
-                  case Left(jobResults: Map[String, Any]) =>
-                    JobResult(success = true, payload = jobResults, request = jobCreatingRequest, errors = List.empty)
-                  case Right(error: String) =>
-                    JobResult(success = false, payload = Map.empty[String, Any], request = jobCreatingRequest, errors = List(error))
+                future
+                  .recover {
+                    case error: AskTimeoutException => Right(Constants.Errors.jobTimeOutError)
+                    case error: Throwable => Right(error.toString)
+                  }
+                  .map[ToResponseMarshallable] {
+                  case result: Either[Map[String, Any], String] =>
+                    val jobResult: JobResult = result match {
+                      case Left(jobResults: Map[String, Any]) =>
+                        JobResult(success = true, payload = jobResults, request = jobCreatingRequest, errors = List.empty)
+                      case Right(error: String) =>
+                        JobResult(success = false, payload = Map.empty[String, Any], request = jobCreatingRequest, errors = List(error))
+                    }
+                    HttpResponse(entity = HttpEntity(ContentType(MediaTypes.`application/json`), Json(DefaultFormats).write(jobResult)))
                 }
-                Json(DefaultFormats).write(jobResult)
-            }
-        }
+              }
+          }
       }
     }
   }
