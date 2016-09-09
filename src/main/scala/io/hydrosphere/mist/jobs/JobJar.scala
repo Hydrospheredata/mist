80510a71b5ec5d335c13aacc122f7e4df0db1e50
root <lenin133@yandex.ru>
2016-09-09 21:27:40 +0300

    fix removed don`t need somefing

--- src/main/scala/io/hydrosphere/mist/jobs/JobJar.scala
+++ src/main/scala/io/hydrosphere/mist/jobs/JobJar.scala
@@ -3,8 +3,6 @@ package io.hydrosphere.mist.jobs
 import java.io.File
 import java.net.{URL, URLClassLoader}
 import io.hydrosphere.mist.contexts.ContextWrapper
-import java.io.BufferInputStream
-import java.io.FileInputStream
 import java.io.FileInputStream
 import org.apache.hadoop.conf._
 import org.apache.hadoop.fs._
@@ -26,15 +24,7 @@ private[mist] class JobJar(jobConfiguration: JobConfiguration, _contextWrapper: 
 
   // Class with job in user jar
   override val cls = try{
-    val jarFile = configuration.jarPath.get.split(':').headOption.getOrElse("") match {
-      case "hdfs" => {
-        val conf = new Configuration()
-        val fileSystem = FileSystem.get(conf)
-        val path = new Path(configuration.jarPath.get)
-        fileSystem.open(path)
-      }
-      case _ => new File(configuration.jarPath.get)
-    }
+    val jarFile = new File(configuration.jarPath.get)
     val classLoader = new URLClassLoader(Array[URL](jarFile.toURI.toURL), getClass.getClassLoader)
     classLoader.loadClass(configuration.className.get)
   } catch {
