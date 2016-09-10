59136a6a0c7a0c584b984a84ce56477e02855c14
root <lenin133@yandex.ru>
2016-09-10 17:36:08 +0300

    hdfs

--- src/main/scala/io/hydrosphere/mist/jobs/JobJar.scala
+++ src/main/scala/io/hydrosphere/mist/jobs/JobJar.scala
@@ -24,8 +24,35 @@ private[mist] class JobJar(jobConfiguration: JobConfiguration, _contextWrapper: 
 
   // Class with job in user jar
   override val cls = try{
-    val jarFile = new File(configuration.jarPath.get)
-    val classLoader = new URLClassLoader(Array[URL](jarFile.toURI.toURL), getClass.getClassLoader)
+
+    val classLoader = configuration.jarPath.get.split(':').headOption.getOrElse("") match {
+      case "hdfs" => {
+        val conf = new Configuration()
+        val fileSystem = FileSystem.get(conf)
+        val path = new Path(configuration.jarPath.get)
+        val localpath = new Path(path.getName())
+        fileSystem.copyToLocalFile(false, path, localpath, true)
+        val jarFile = new File(path.getName())
+        //val istream = fileSystem.open(path)
+        //val jarFile = istream.getFileDescriptor()
+       /*
+        val uriPath = path.toUri()
+        val urlPath = uriPath.toURL()
+        val jarFile = urlPath.getFile
+        println(jarFile)
+        val tmpFile = new File("job.tmp")
+        val fos = new FileOutputStream(tmpFile)
+        fos.write(istream.getBytes())
+        */
+        new URLClassLoader(Array[URL](jarFile.toURI.toURL), getClass.getClassLoader)    
+      }
+      case _ => {
+        val jarFile = new File(configuration.jarPath.get)
+        new URLClassLoader(Array[URL](jarFile.toURI.toURL), getClass.getClassLoader)    
+      }
+    }
+
+    
     classLoader.loadClass(configuration.className.get)
   } catch {
     case e: Throwable =>
