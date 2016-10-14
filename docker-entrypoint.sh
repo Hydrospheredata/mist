4a4e0b9317d04470d1bcaaee363cd529911109f9
Andrey Petrov <peanig@gmail.com>
2016-10-14 11:28:08 +0300

    add separate docker containers as workers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,11 @@ if [ "$1" = 'tests' ]; then
   ./sbt/sbt -DsparkVersion=${SPARK_VERSION} -Dconfig.file=src/test/resources/tests-${SPARK_VERSION}.conf "project examples" package "project mist" test
   bash
 elif [ "$1" = 'mist' ]; then
+  export IP=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
+  echo "$IP    master" >> /etc/hosts
   ./bin/mist start master --config configs/docker.conf --jar target/scala-*/mist-assembly-*.jar
+elif [ "$1" = 'worker' ]; then 
+  ./bin/mist start worker --runner local --namespace $2 --config configs/docker.conf --jar target/scala-*/mist-assembly-*.jar
 elif [ "$1" = 'dev' ]; then
   ./sbt/sbt -DsparkVersion=${SPARK_VERSION} assembly
   ./sbt/sbt -DsparkVersion=${SPARK_VERSION} "project examples" package
