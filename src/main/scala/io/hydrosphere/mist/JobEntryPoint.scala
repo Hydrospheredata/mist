cfd4d92ee4d396c1668871245abbeefc7b98295f
Konstantin Makarychev <mrsimpson@inbox.ru>
2016-10-26 17:39:30 +0300

    Run jobs via CLI

--- src/main/scala/io/hydrosphere/mist/JobEntryPoint.scala
+++ src/main/scala/io/hydrosphere/mist/JobEntryPoint.scala
@@ -8,17 +8,17 @@ import spray.json.pimpString
 private[mist] object JobEntryPoint extends App with Logger with JsonFormatSupport{
 
   if (args.length < 4) {
-    logger.error("`path` `className` `name` `externalId` arguments is required")
+    logger.error("`path` `className` `name` arguments are required")
     System.exit(1)
   }
 
   implicit val system = ActorSystem("mist", MistConfig.Akka.Worker.settings)
 
   val contextNode =
-    if(args.length == 5) {
+    if (args.length == 5) {
       val json = args(4).toString.parseJson
       system.actorOf(Props(new JobRunnerNode(args(0), args(1), args(2), args(3), json.convertTo[Map[String, Any]])), name = "JobStarter")
-    }
-    else
+    } else if (args.length == 4) {
       system.actorOf(Props(new JobRunnerNode(args(0), args(1), args(2), args(3), Map().empty)), name = "JobStarter")
+    }
 }
