925157fadd6a258433c07a3e8c7b7397c87d37f1
IceKhan13 <iskandar.sitdikov@gmail.com>
2017-04-15 15:43:13 +0300

    fix

--- examples/src/main/scala/NaiveBayesJob_Spark2.scala
+++ examples/src/main/scala/NaiveBayesJob_Spark2.scala
@@ -31,7 +31,7 @@ object NaiveBayesJob extends MLMistJob with SQLSupport {
 
     val pipeline = PipelineLoader.load(modelPath)
     val data = LocalData(
-      LocalDataColumn("features", features.map(x => Vectors.dense(x.toArray)))
+      LocalDataColumn("features", features)
     )
 
     val result: LocalData = pipeline.transform(data)
