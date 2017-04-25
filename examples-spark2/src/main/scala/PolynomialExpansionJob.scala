1fdf650eeca59cbbc96c292c05f65bebf9069fe3
dos65 <qtankle@gmail.com>
2017-04-25 13:14:04 +0300

    ml exmaples - fix polynominal expansion

--- examples-spark2/src/main/scala/PolynomialExpansionJob.scala
+++ examples-spark2/src/main/scala/PolynomialExpansionJob.scala
@@ -3,7 +3,7 @@ import io.hydrosphere.mist.lib.spark2.ml._
 
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.feature.PolynomialExpansion
-import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.linalg.{DenseVector, Vectors}
 
 object PolynomialExpansionJob extends MLMistJob with SQLSupport  {
   def train(savePath: String): Map[String, Any] = {
@@ -31,11 +31,12 @@ object PolynomialExpansionJob extends MLMistJob with SQLSupport  {
     import LocalPipelineModel._
 
     val pipeline = PipelineLoader.load(modelPath)
-    val data = LocalData(
-      LocalDataColumn("features", features)
-    )
+    val data = LocalData(LocalDataColumn("features", features))
 
-    val result: LocalData = pipeline.transform(data)
-    Map("result" -> result.select("features", "polyFeatures").toMapList)
+    val result = pipeline.transform(data)
+    val column = result.column("polyFeatures").map(_.asInstanceOf[LocalDataColumn[DenseVector]])
+    val response = column.map(c => c.data.map(_.toArray))
+
+    Map("result" -> response)
   }
 }
