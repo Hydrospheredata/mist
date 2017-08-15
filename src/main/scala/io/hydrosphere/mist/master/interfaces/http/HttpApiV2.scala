9649ceb31c183d9bfe824e87fdaf8cb36bdcd20a
Pavel Borobov <blvp.me@gmail.com>
2017-08-15 20:28:05 +0300

    fix: handle not existing job log file

--- src/main/scala/io/hydrosphere/mist/master/interfaces/http/HttpApiV2.scala
+++ src/main/scala/io/hydrosphere/mist/master/interfaces/http/HttpApiV2.scala
@@ -1,10 +1,10 @@
 package io.hydrosphere.mist.master.interfaces.http
 
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshalling.{ToEntityMarshaller, ToResponseMarshallable}
-import akka.http.scaladsl.model.{StatusCodes, StatusCode, HttpResponse}
+import akka.http.scaladsl.model.{HttpEntity, HttpResponse, StatusCode, StatusCodes}
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.server.directives.ParameterDirectives
-
 import io.hydrosphere.mist.jobs.JobDetails
 import io.hydrosphere.mist.jobs.JobDetails.Source
 import io.hydrosphere.mist.master.data.ContextsStorage
@@ -224,7 +224,10 @@ object HttpV2Routes {
     } ~
     path( root / "jobs" / Segment / "logs") { jobId =>
       get {
-        getFromFile(master.logStorageMappings.pathFor(jobId).toFile)
+        master.logStorageMappings.pathFor(jobId).toFile match {
+          case file if file.exists => getFromFile(file)
+          case _ => complete { HttpResponse(StatusCodes.OK, entity=HttpEntity.Empty) }
+        }
       }
     } ~
     path( root / "jobs" / Segment ) { jobId =>
