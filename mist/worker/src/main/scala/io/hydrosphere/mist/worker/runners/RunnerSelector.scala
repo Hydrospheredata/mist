dc2b8764472a13485d55c6a62c3b0bb30d8a887a
Borobov Pavel <blvp.me@gmail.com>
2017-09-05 17:58:25 +0300

    added test case for runner selector

--- mist/worker/src/main/scala/io/hydrosphere/mist/worker/runners/RunnerSelector.scala
+++ mist/worker/src/main/scala/io/hydrosphere/mist/worker/runners/RunnerSelector.scala
@@ -14,6 +14,7 @@ class SimpleRunnerSelector extends RunnerSelector {
   override def selectRunner(file: File): JobRunner = file.getName match {
     case fname if fname.endsWith(".py") => new PythonRunner(file)
     case fname if fname.endsWith(".jar") => new ScalaRunner(file)
+    case f => throw new IllegalArgumentException(s"Unknown file type $f is passed, cannot select runner")
   }
 
 }
