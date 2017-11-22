5d0b87a5c8d8596877a53c7802cf0eaf1dad8cf2
dos65 <qtankle@gmail.com>
2017-11-22 17:49:50 +0300

    fix warning

--- mist/master/src/main/scala/io/hydrosphere/mist/master/interfaces/http/models.scala
+++ mist/master/src/main/scala/io/hydrosphere/mist/master/interfaces/http/models.scala
@@ -55,7 +55,7 @@ object HttpJobArg {
   def convert(argType: ArgType): HttpJobArg = {
     val t = argType.getClass.getSimpleName.replace("$", "")
     val typeArgs = argType match {
-      case x@(MInt | MDouble | MString | MAny) => Seq.empty
+      case x@(MBoolean | MInt | MDouble | MString | MAny) => Seq.empty
       case x: MMap => Seq(x.k, x.v).map(HttpJobArg.convert)
       case x: MList => Seq(HttpJobArg.convert(x.v))
       case x: MOption => Seq(HttpJobArg.convert(x.v))
