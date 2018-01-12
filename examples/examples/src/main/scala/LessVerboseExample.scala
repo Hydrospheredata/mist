8f21057fe8c5afaafc90f76ca5afc9307b211f08
dos65 <qtankle@gmail.com>
2018-01-12 22:53:08 +0300

    revert example

--- examples/examples/src/main/scala/LessVerboseExample.scala
+++ examples/examples/src/main/scala/LessVerboseExample.scala
@@ -4,24 +4,26 @@ import mist.api.args.ArgExtractor
 import mist.api.encoding.DefaultEncoders._
 import org.apache.spark.SparkContext
 
-case class NumArg(value: Int, mult: Int)
 case class Args(
-  numbers: Seq[NumArg]
-)
+  numbers: Seq[Int],
+  multiplier: Option[Int]
+) {
+
+  def mult: Int = multiplier.getOrElse(2)
+}
 
 object LessVerboseExample extends MistFn[Array[Int]] {
 
   import MistExtras._
 
-  implicit val argExt = ArgExtractor.rootFor[Args]
+  implicit val extractor = ArgExtractor.rootFor[Args]
 
   override def handle = (arg[Args] & mistExtras).onSparkContext(
     (args: Args, extras: MistExtras, sc: SparkContext) => {
       import extras._
 
       logger.info(s"Heello from $jobId")
-      //sc.parallelize(args.numbers).map(a => a.value * a.mult).collect()
-      Array.empty[Int]
+      sc.parallelize(args.numbers).map(_ * args.mult).collect()
     })
 
 }
