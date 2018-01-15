e4ec97393e9bcdfa650f2ab1de08050dfd45ab02
Borobov Pavel <blvp.me@gmail.com>
2018-01-15 17:14:16 +0300

    rename class

--- mist-tests/scala/io.hydrosphere.mist/ExamplesSpecSpark.scala
+++ mist-tests/scala/io.hydrosphere.mist/ExamplesSpecSpark.scala
@@ -2,7 +2,7 @@ package io.hydrosphere.mist
 
 import org.scalatest.{FunSpec, Matchers}
 
-class ExamplesSpecSpark1 extends FunSpec with MistItTest with Matchers {
+class ExamplesSpecSpark extends FunSpec with MistItTest with Matchers {
 
   val interface = MistHttpInterface("localhost", 2004)
 
