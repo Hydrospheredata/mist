e7c168e8725f3639f171b3411140c1b3624cf23b
Borobov Pavel <blvp.me@gmail.com>
2018-01-15 16:48:48 +0300

    make integration test reliable for http call to mist for both spark 1 and spark 2

--- mist-tests/scala/io.hydrosphere.mist/ExamplesSpecSpark.scala
+++ mist-tests/scala/io.hydrosphere.mist/ExamplesSpecSpark.scala
@@ -4,18 +4,14 @@ import org.scalatest.{FunSpec, Matchers}
 
 class ExamplesSpecSpark1 extends FunSpec with MistItTest with Matchers {
 
-  if (isSpark1) {
-    val interface = MistHttpInterface("localhost", 2004)
+  val interface = MistHttpInterface("localhost", 2004)
 
-    it("spark-ctx-example") {
-      val result = interface.runJob("spark-ctx-example",
-          "numbers" -> List(1, 2, 3),
-          "multiplier" -> 2
-      )
+  it("spark-ctx-example") {
+    val result = interface.runJob("spark-ctx-example",
+        "numbers" -> List(1, 2, 3),
+        "multiplier" -> 2
+    )
 
-      result.success shouldBe true
-    }
-    
+    result.success shouldBe true
   }
-
 }
