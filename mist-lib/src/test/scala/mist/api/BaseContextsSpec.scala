cd4b839fa0ce8d14e7762c6275e0548664da11b1
dos65 <qtankle@gmail.com>
2018-03-20 15:48:12 +0300

    fix tests

--- mist-lib/src/test/scala/mist/api/BaseContextsSpec.scala
+++ mist-lib/src/test/scala/mist/api/BaseContextsSpec.scala
@@ -2,12 +2,13 @@ package mist.api
 
 import io.hydrosphere.mist.api.{RuntimeJobInfo, SetupConfiguration}
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.hive.HiveContext
 import org.scalatest.{FunSpec, Matchers}
 
 class BaseContextsSpec extends FunSpec with Matchers with TestSparkContext {
 
-  import BaseContexts._
+  import Contexts._
   import mist.api.args.ArgsInstances._
 
   it("for spark context") {
@@ -32,23 +33,15 @@ class BaseContextsSpec extends FunSpec with Matchers with TestSparkContext {
     this.getClass.getClassLoader.getResource(path).getPath
   }
 
-  it("for only hive context") {
-    val spJob = onHiveContext((hiveCtx: HiveContext) => {
-      val df = hiveCtx.read.json(pathToResource("hive_job_data.json"))
-      df.registerTempTable("temp")
-      hiveCtx.sql("SELECT MAX(age) AS avg_age FROM temp")
-        .take(1)(0).getLong(0)
-    })
-    spJob.invoke(testCtx())
-    val res = spJob.invoke(testCtx())
-    res shouldBe JobSuccess(30)
-  }
+  it("session with hive") {
+    val spJob = onSparkSessionWithHive((spark: SparkSession) => {
+      val df = spark.read.json(pathToResource("hive_job_data.json"))
+      df.createOrReplaceTempView("temp")
+      df.cache()
+      spark.sql("DROP TABLE IF EXISTS temp_hive")
+      spark.table("temp").write.saveAsTable("temp_hive")
 
-  it("allow more than one hive context") {
-    val spJob = onHiveContext((hiveCtx: HiveContext) => {
-      val df = hiveCtx.read.json(pathToResource("hive_job_data.json"))
-      df.registerTempTable("temp")
-      hiveCtx.sql("SELECT MAX(age) AS avg_age FROM temp")
+      spark.sql("SELECT MAX(age) AS avg_age FROM temp_hive")
         .take(1)(0).getLong(0)
     })
     spJob.invoke(testCtx())
--- mist/master/src/test/scala/io/hydrosphere/mist/master/execution/workers/WorkerRunnerSpec.scala
+++ mist/master/src/test/scala/io/hydrosphere/mist/master/execution/workers/WorkerRunnerSpec.scala
@@ -1,9 +1,10 @@
 package io.hydrosphere.mist.master.execution.workers
 
+import io.hydrosphere.mist.core.CommonData.WorkerInitInfo
 import io.hydrosphere.mist.core.MockitoSugar
-import io.hydrosphere.mist.master.{ActorSpec, FilteredException, TestData}
+import io.hydrosphere.mist.master.execution.workers.starter.{NonLocal, WorkerStarter}
 import io.hydrosphere.mist.master.execution.{SpawnSettings, workers}
-import io.hydrosphere.mist.master.models.ContextConfig
+import io.hydrosphere.mist.master.{ActorSpec, FilteredException, TestData}
 import io.hydrosphere.mist.utils.akka.ActorRegHub
 import org.mockito.Mockito._
 import org.scalatest.concurrent.Eventually
@@ -12,80 +13,86 @@ import org.scalatest.time.{Seconds, Span}
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future, Promise}
 
-//class WorkerRunnerSpec extends ActorSpec("worker-runner") with TestData with MockitoSugar with Eventually {
-//
-//  describe("default runner") {
-//
-//    def mkSpawnSettings(cmd: RunnerCmd): SpawnSettings = SpawnSettings(
-//      runnerCmd = cmd,
-//      timeout = 10 seconds,
-//      readyTimeout = 10 seconds,
-//      akkaAddress = "akkaAddr",
-//      logAddress = "logAddr",
-//      httpAddress = "httpAddr",
-//      maxArtifactSize = 100L
-//    )
-//
-//    it("should run worker") {
-//      val runnerCmd = mock[RunnerCmd]
-//      val regHub = mock[ActorRegHub]
-//      when(regHub.waitRef(any[String], any[Duration])).thenSuccess(null)
-//
-//      val termination = Promise[Unit]
-//      val runner = new workers.WorkerRunner.DefaultRunner(
-//        spawn = mkSpawnSettings(runnerCmd),
-//        regHub = regHub,
-//        connect = (_, _, _, _) => Future.successful(WorkerConnection("id", null, workerLinkData, termination.future))
-//      )
-//
-//      Await.result(runner("id", FooContext), Duration.Inf)
-//      verify(runnerCmd).runWorker(any[String], any[ContextConfig])
-//
-//      termination.success(())
-//
-//      eventually(timeout(Span(3, Seconds))) {
-//        verify(runnerCmd).onStop(any[String])
-//      }
-//    }
-//
-//    it("should call onStop if await ref was failed") {
-//      val runnerCmd = mock[RunnerCmd]
-//      val regHub = mock[ActorRegHub]
-//      when(regHub.waitRef(any[String], any[Duration])).thenFailure(FilteredException())
-//
-//      val runner = new workers.WorkerRunner.DefaultRunner(
-//        spawn = mkSpawnSettings(runnerCmd),
-//        regHub = regHub,
-//        connect = (_, _, _, _) => Future.failed(FilteredException())
-//      )
-//
-//      intercept[Throwable] {
-//        Await.result(runner("id", FooContext), Duration.Inf)
-//      }
-//
-//      eventually(timeout(Span(3, Seconds))) {
-//        verify(runnerCmd).onStop(any[String])
-//      }
-//    }
-//
-//    it("should call onStop if connect was failed") {
-//      val runnerCmd = mock[RunnerCmd]
-//      val regHub = mock[ActorRegHub]
-//      when(regHub.waitRef(any[String], any[Duration])).thenSuccess(null)
-//
-//      val runner = new workers.WorkerRunner.DefaultRunner(
-//        spawn = mkSpawnSettings(runnerCmd),
-//        regHub = regHub,
-//        connect = (_, _, _, _) => Future.failed(FilteredException())
-//      )
-//
-//      intercept[Throwable] {
-//        Await.result(runner("id", FooContext), Duration.Inf)
-//      }
-//
-//      eventually(timeout(Span(3, Seconds))) {
-//        verify(runnerCmd).onStop(any[String])
-//      }
-//    }
-//  }
-//}
+class WorkerRunnerSpec extends ActorSpec("worker-runner") with TestData with MockitoSugar with Eventually {
+
+  describe("default runner") {
+
+    def mkSpawnSettings(starter: WorkerStarter): SpawnSettings = SpawnSettings(
+      runnerCmd = starter,
+      timeout = 10 seconds,
+      readyTimeout = 10 seconds,
+      akkaAddress = "akkaAddr",
+      logAddress = "logAddr",
+      httpAddress = "httpAddr",
+      maxArtifactSize = 100L
+    )
+
+    def mockStarter: WorkerStarter = {
+      val starter = mock[WorkerStarter]
+      when(starter.onStart(any[String], any[WorkerInitInfo])).thenReturn(NonLocal)
+      starter
+    }
+
+    it("should run worker") {
+      val starter = mockStarter
+      val regHub = mock[ActorRegHub]
+      when(regHub.waitRef(any[String], any[Duration])).thenSuccess(null)
+
+      val termination = Promise[Unit]
+      val runner = new workers.WorkerRunner.DefaultRunner(
+        spawn = mkSpawnSettings(starter),
+        regHub = regHub,
+        connect = (_, _, _, _) => Future.successful(WorkerConnection("id", null, workerLinkData, termination.future))
+      )
+
+      Await.result(runner("id", FooContext), Duration.Inf)
+      verify(starter).onStart(any[String], any[WorkerInitInfo])
+
+      termination.success(())
+
+      eventually(timeout(Span(3, Seconds))) {
+        verify(starter).onStop(any[String])
+      }
+    }
+
+    it("should call onStop if await ref was failed") {
+      val runnerCmd = mock[WorkerStarter]
+      val regHub = mock[ActorRegHub]
+      when(regHub.waitRef(any[String], any[Duration])).thenFailure(FilteredException())
+
+      val runner = new workers.WorkerRunner.DefaultRunner(
+        spawn = mkSpawnSettings(runnerCmd),
+        regHub = regHub,
+        connect = (_, _, _, _) => Future.failed(FilteredException())
+      )
+
+      intercept[Throwable] {
+        Await.result(runner("id", FooContext), Duration.Inf)
+      }
+
+      eventually(timeout(Span(3, Seconds))) {
+        verify(runnerCmd).onStop(any[String])
+      }
+    }
+
+    it("should call onStop if connect was failed") {
+      val starter = mockStarter
+      val regHub = mock[ActorRegHub]
+      when(regHub.waitRef(any[String], any[Duration])).thenSuccess(null)
+
+      val runner = new workers.WorkerRunner.DefaultRunner(
+        spawn = mkSpawnSettings(starter),
+        regHub = regHub,
+        connect = (_, _, _, _) => Future.failed(FilteredException())
+      )
+
+      intercept[Throwable] {
+        Await.result(runner("id", FooContext), Duration.Inf)
+      }
+
+      eventually(timeout(Span(3, Seconds))) {
+        verify(starter).onStop(any[String])
+      }
+    }
+  }
+}
