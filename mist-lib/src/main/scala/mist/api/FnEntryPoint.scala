f14ca85b680c2f340b370053ce385139516b97b3
dos65 <qtankle@gmail.com>
2018-04-25 15:52:32 +0300

    fix python functions

--- mist-lib/src/main/scala/mist/api/FnEntryPoint.scala
+++ mist-lib/src/main/scala/mist/api/FnEntryPoint.scala
@@ -1,6 +1,6 @@
 package mist.api
 
-import mist.api.data.{JsData, JsMap}
+import mist.api.data.{JsData, JsMap, Json4sConversion}
 import org.apache.spark.SparkContext
 import org.slf4j.LoggerFactory
 
@@ -15,7 +15,7 @@ trait FnEntryPoint {
     val sc = SparkContext.getOrCreate()
 
     val inputData = args.headOption match {
-      case Some(s) => JsData.parseRoot(s) match {
+      case Some(s) => Json4sConversion.parseRoot(s) match {
         case Success(js) => js
         case Failure(e) =>
           logger.error("Couldn't parse input data as json map", e)
@@ -29,7 +29,7 @@ trait FnEntryPoint {
     execute(fnContext) match {
       case Success(output) =>
         logger.info("Function completed successfully")
-        val formatted = JsData.formattedString(output)
+        val formatted = Json4sConversion.formattedString(output)
         logger.info("Output data:\n" + formatted)
       case Failure(err) =>
         logger.error("Function completed with error", err)
--- mist-lib/src/main/scala/mist/api/data/JsData.scala
+++ mist-lib/src/main/scala/mist/api/data/JsData.scala
@@ -1,5 +1,6 @@
 package mist.api.data
 
+import scala.collection.mutable
 import scala.util._
 
 /**
@@ -25,7 +26,7 @@ case object JsTrue extends JsData {
   override def toString: String = "true"
 }
 case object JsFalse extends JsData {
-  override def toString: String = "true"
+  override def toString: String = "false"
 }
 
 object JsBoolean {
@@ -143,46 +144,4 @@ object JsData {
     case _: java.util.Optional[_] => JsNull
   }
 
-  /** For running mist jobs directly from spark-submit **/
-  def parse(s: String): Try[JsData] = {
-    import org.json4s._
-
-    def translateAst(in: JValue): JsData = in match {
-      case JNothing => JsNull //???
-      case JNull => JsNull
-      case JString(s) => JsString(s)
-      case JDouble(d) => JsNumber(d)
-      case JDecimal(d) => JsNumber(d)
-      case JInt(i) => JsNumber(i)
-      case JBool(v) => JsBoolean(v)
-      case JObject(fields) => JsMap(fields.map({case (k, v) => k -> translateAst(v)}): _*)
-      case JArray(elems) => JsList(elems.map(translateAst))
-    }
-
-    Try(org.json4s.jackson.JsonMethods.parse(s, useBigDecimalForDouble = true)).map(json4sJs => translateAst(json4sJs))
-  }
-
-  def parseRoot(s: String): Try[JsMap] = parse(s).flatMap {
-    case m:JsMap => Success(m)
-    case _ => Failure(new IllegalArgumentException(s"Couldn't parse js object from input: $s"))
-  }
-
-  def formattedString(js: JsData): String = {
-    import org.json4s._
-
-    def translateAst(in: JsData): JValue = in match {
-      case JsNull => JNull
-      case JsString(s) => JString(s)
-      case JsNumber(d) =>
-        d.toBigIntExact() match {
-          case Some(x) => JInt(x)
-          case None => JDecimal(d)
-        }
-      case JsTrue => JBool(true)
-      case JsFalse => JBool(false)
-      case JsMap(fields) => JObject(fields.map({case (k, v) => k -> translateAst(v)}).toList)
-      case JsList(elems) => JArray(elems.map(translateAst).toList)
-    }
-    org.json4s.jackson.JsonMethods.pretty(translateAst(js))
-  }
 }
