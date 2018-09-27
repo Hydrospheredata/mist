de62967f8e827d0991bde8cfe3e0c460ee8271d3
dos65 <qtankle@gmail.com>
2018-09-27 03:08:50 +0300

    aws - nginx config

--- cloud_install/aws/install.sh
+++ cloud_install/aws/install.sh
@@ -46,29 +46,28 @@ SPARK_HOME=/opt/spark /opt/mist/bin/mist-master start
 htpasswd -b -c /etc/nginx/.htpasswd $MIST_LOGIN $MIST_PASSWORD
 cat << EOF > /etc/nginx/sites-enabled/default
 server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
 
-	server_name _;
+    server_name _;
 
-	location / {
-	   proxy_pass http://127.0.0.1:2004/;
-     auth_basic "Restricted";
-     auth_basic_user_file /etc/nginx/.htpasswd;
+    location / {
+        proxy_pass http://127.0.0.1:2004/;
+        auth_basic "Restricted";
+        auth_basic_user_file /etc/nginx/.htpasswd;
 	}
 
 	location /v2/api/ws {
-	    proxy_pass         http://127.0.0.1:2004/v2/api/ws;
-	    proxy_http_version 1.1;
-      proxy_set_header Upgrade \$http_upgrade;
-      proxy_set_header Connection "upgrade";
-      auth_basic "Restricted";
-      auth_basic_user_file /etc/nginx/.htpasswd;
-	}
-
+        proxy_pass http://127.0.0.1:2004/v2/api/ws;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+        auth_basic "Restricted";
+        auth_basic_user_file /etc/nginx/.htpasswd;
+    }
 }
 EOF
 service nginx restart
