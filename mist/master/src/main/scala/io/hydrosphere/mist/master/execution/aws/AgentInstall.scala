8dd98189734ed27bec25a818b25f8d6de452844e
dos65 <qtankle@gmail.com>
2018-09-27 23:05:55 +0300

    remove something

--- mist/master/src/main/scala/io/hydrosphere/mist/master/execution/aws/AgentInstall.scala
+++ mist/master/src/main/scala/io/hydrosphere/mist/master/execution/aws/AgentInstall.scala
@@ -41,38 +41,3 @@ object AgentInstall {
   }
 
 }
-
-object Test {
-
-  def main(args: Array[String]): Unit = {
-    val jarsDir = Paths.get("/home/dos65/projects/mist/target/mist-1.0.0-RC17")
-    val realDir = jarsDir.toAbsolutePath.toRealPath()
-
-    val host = "ec2-18-196-156-203.eu-central-1.compute.amazonaws.com"
-    val sshUser = "hadoop"
-    val agentId= "j-2NVSS1JAKYOEU"
-
-    val akkaAddress = "blabla.com:2005"
-
-    val accessKey = "AKIA5XGMCUEIJ3NOW6Y3"
-    val secretKey = "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"
-    val region = "eu-central-1"
-
-
-    val transfer = TransferParams(
-      realDir.resolve("mist-agent.jar").toString,
-      realDir.resolve("mist-worker.jar").toString,
-      s"/home/$sshUser/mist-agent"
-    )
-    val agentRunParams = AgentRunParams(
-      agentId, akkaAddress, accessKey, secretKey, region, agentId
-    )
-    val cmds = AgentInstall.sshCommands(transfer, agentRunParams)
-    val out = new SSHClient(host, sshUser, "/home/dos65/.ssh/mist_key").install(cmds)
-
-    out match {
-      case scala.util.Failure(exception) => exception.printStackTrace()
-      case scala.util.Success(value) => println("OOK")
-    }
-  }
-}
