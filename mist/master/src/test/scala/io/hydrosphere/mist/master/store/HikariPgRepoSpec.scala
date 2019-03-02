36a29c88e43187acabcde5fd7f23136956e923e6
Andrew G. Saushkin <asaushkin@gmail.com>
2019-03-02 17:38:11 +0300

    Mark HikariPgRepoSpec abstract while Travis CI
    HikariPgRepoSpec can't pass Travis CI tests. Mark it abstract
    while Travis CI will be ready for it.

--- mist/master/src/test/scala/io/hydrosphere/mist/master/store/HikariPgRepoSpec.scala
+++ mist/master/src/test/scala/io/hydrosphere/mist/master/store/HikariPgRepoSpec.scala
@@ -1,6 +1,6 @@
 package io.hydrosphere.mist.master.store
 
 // TODO: Create testing environment for PostgreSQL and remove an abstract qualifier
-class HikariPgRepoSpec extends HikariRepoSpec("org.postgresql.Driver",
+abstract class HikariPgRepoSpec extends HikariRepoSpec("org.postgresql.Driver",
   "jdbc:postgresql:mist", "postgres", "") {
 }
