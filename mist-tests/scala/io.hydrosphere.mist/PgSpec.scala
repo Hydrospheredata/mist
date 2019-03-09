77a83b140da7f6cb33459147d708f0908144de0c
Vadim Chelyshov <qtankle@gmail.com>
2019-03-09 21:15:16 +0300

    pg - integration tests

---
+++ mist-tests/scala/io.hydrosphere.mist/PgSpec.scala
@@ -0,0 +1,98 @@
+package io.hydrosphere.mist
+
+import com.zaxxer.hikari.HikariConfig
+import io.hydrosphere.mist.core.CommonData.{Action, JobParams}
+import io.hydrosphere.mist.master.JobDetails
+import io.hydrosphere.mist.master.JobDetails.Source
+import io.hydrosphere.mist.master.store.{HikariDataSourceTransactor, HikariJobRepository}
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Seconds, Span}
+import org.scalatest.{BeforeAndAfterAll, FunSpec, Matchers}
+import mist.api.data.JsMap
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.Duration
+
+class PgSpec extends FunSpec
+  with BeforeAndAfterAll
+  with Matchers
+  with Eventually {
+  
+  implicit override val patienceConfig = PatienceConfig(timeout = scaled(Span(2, Seconds)), interval = scaled(Span(1, Seconds)))
+  
+  var pgContainer: TestContainer = _
+  var tr: HikariDataSourceTransactor = _
+  var repo: HikariJobRepository = _
+  
+  override def beforeAll = {
+    pgContainer = TestContainer.run(DockerImage("postgres","latest"), Map(5432 -> 5432))
+    
+    val hikariConfig = new HikariConfig()
+    hikariConfig.setDriverClassName("org.postgresql.Driver")
+    hikariConfig.setJdbcUrl("jdbc:postgresql:postgres")
+    hikariConfig.setUsername("postgres")
+    hikariConfig.setPassword("postgres")
+  
+    tr = new HikariDataSourceTransactor(hikariConfig)
+    repo = new HikariJobRepository(tr)
+  }
+  override def afterAll = {
+    pgContainer.close()
+    tr.shutdown()
+  }
+  
+  private def await[A](f: Future[A]): A = Await.result(f, Duration.Inf)
+  
+  
+  it("remove") {
+    val details = fixtureJobDetails("id")
+    await(repo.remove(details.jobId))
+    await(repo.get(details.jobId)) shouldBe None
+  }
+  
+  it("update") {
+    val details = fixtureJobDetails("id")
+    await(repo.update(details))
+    await(repo.get(details.jobId)) shouldBe Some(details)
+  }
+  
+  it("clear") {
+    (1 to 10).foreach(i => await(repo.update(fixtureJobDetails(s"jobId $i"))))
+    await(repo.clear())
+    await(repo.getAll(10, 0)).size shouldBe 0
+  }
+  
+  it("filter by status") {
+    (1 to 2).foreach(i => {
+      val details = fixtureJobDetails(s"jobId $i", JobDetails.Status.Started)
+      await(repo.update(details))
+    })
+    await(repo.update(fixtureJobDetails("ignore")))
+    
+    val runningJobs = repo.filteredByStatuses(List(JobDetails.Status.Started))
+    await(runningJobs).size shouldBe 2
+  }
+  
+  it("decode failure") {
+    val details = fixtureJobDetails("failed").withFailure("Test Error")
+    await(repo.update(details))
+    await(repo.get("failed")) shouldBe Some(details)
+  }
+  
+  
+  // Helper functions
+  private def fixtureJobDetails(
+    jobId: String,
+    status: JobDetails.Status = JobDetails.Status.Initialized): JobDetails = {
+    JobDetails(
+      params = JobParams("path", "className", JsMap.empty, Action.Execute),
+      jobId = jobId,
+      source = Source.Http,
+      function = "function",
+      context = "context",
+      externalId = None,
+      status = status
+    )
+  }
+}
+
--- mist/master/src/test/scala/io/hydrosphere/mist/master/store/HikariPgRepoSpec.scala
+++
@@ -1,6 +0,0 @@
-package io.hydrosphere.mist.master.store
-
-// TODO: Create testing environment for PostgreSQL and remove an abstract qualifier
-abstract class HikariPgRepoSpec extends HikariRepoSpec("org.postgresql.Driver",
-  "jdbc:postgresql:mist", "postgres", "") {
-}
